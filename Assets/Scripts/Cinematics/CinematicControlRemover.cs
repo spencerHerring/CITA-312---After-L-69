3485afe9497d311bee7ecef6bd26c2a12a6b4a60
SpencerHerring <herrin348@morrisville.edu>
2019-10-12 21:38:37 -0400

    L69, game broke had to take commit L69

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+        GameObject player;
+        // Start is called before the first frame update
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindWithTag("Player");
+        }
+
+        void DisableControl(PlayableDirector pd)
+        { 
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+
+}
