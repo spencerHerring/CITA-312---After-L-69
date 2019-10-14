89e810b07dbf70c585590b0abc59cf9ee4105e95
SpencerHerring <herrin348@morrisville.edu>
2019-10-14 11:55:39 -0400

    S8 L79 adding the Saving system and Wrapper

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string deafualtSaveFile = "save";
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            //call to saving system
+            GetComponent<SavingSystem>().Save(deafualtSaveFile);
+        }
+
+        private void Load()
+        {
+            //call to saving system 
+            GetComponent<SavingSystem>().Load(deafualtSaveFile);
+        }
+    }
+}
+
